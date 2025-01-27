e501c4e082d2b07fef0d66d9857c28c9233d4e92
Wuchieh <q20001116@gmail.com>
2025-01-27 14:24:57 +0800

    add error

---
+++ error.go
@@ -0,0 +1,40 @@
+package socket
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var (
+	ErrToListEmpty        = errors.New("to list is empty")
+	ErrToExceptDuplicates = errors.New("the to list duplicates the value of the except list")
+)
+
+type EmitError struct {
+	Member *Member
+	Err    error
+}
+
+type EmitErrors []EmitError
+
+func (e EmitErrors) Error() string {
+	msg := make([]string, 0, len(e))
+
+	for _, emitError := range e {
+		m := fmt.Sprintf("member: %s, error: %s", emitError.Member.id.String(), emitError.Err.Error())
+		msg = append(msg, m)
+	}
+
+	return strings.Join(msg, "\n")
+}
+
+func addEmitErr(src error, dst EmitError) EmitErrors {
+	var err EmitErrors
+	if errors.As(src, &err) {
+		err = append(err, dst)
+		return err
+	} else {
+		return EmitErrors{dst}
+	}
+}
