339defb8097c90b3c9a59be2bff9234e334b4bc5
Wuchieh <q20001116@gmail.com>
2025-02-10 11:43:30 +0800

    feat: add GetMembers and Broadcast func

--- utils.go
+++ utils.go
@@ -1,6 +1,7 @@
 package socket
 
 import (
+	"github.com/gorilla/websocket"
 	"sync"
 )
 
@@ -25,4 +26,50 @@ func LeaveRoom(room string, s *Socket, m *Member) {
 	s.roomRemoveMember(room, m)
 
 	m.leaveRoom(room)
+}
+
+func GetMembers(m *sync.Map) []*Member {
+	if m == nil {
+		return nil
+	}
+
+	var members []*Member
+
+	m.Range(func(key, value interface{}) bool {
+		member := value.(*Member)
+		members = append(members, member)
+		return true
+	})
+
+	return members
+}
+
+func Broadcast(m []*Member, event string, data any) error {
+	res := Response{
+		Event: event,
+		Data:  data,
+	}
+
+	preparedMsg, err := websocket.NewPreparedMessage(websocket.TextMessage, res.GetByte())
+	if err != nil {
+		return err
+	}
+
+	var eErr error
+	var wg sync.WaitGroup
+
+	wg.Add(len(m))
+	for _, member := range m {
+		go func(member *Member) {
+			if err := member.c.WritePreparedMessage(preparedMsg); err != nil {
+				eErr = addEmitErr(eErr, EmitError{
+					Member: member,
+					Err:    err,
+				})
+			}
+		}(member)
+	}
+
+	wg.Wait()
+	return eErr
 }
